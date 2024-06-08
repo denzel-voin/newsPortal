5983e90f5ec936a2fa51ec13753341dc3e2e3d36
Denis Voinovich <denzelvojn@gmail.com>
2024-06-08 18:26:19 +0300

    add server

--- src/index.js
+++ src/index.js
@@ -1,75 +1,56 @@
-const apiKey = 'apiKey=16cd62b7e752472586a561b2daab6ac9';
-const apiUrl = 'https://newsapi.org/v2/top-headlines?country=us&category=';
+const apiKey = '16cd62b7e752472586a561b2daab6ac9';
+const apiUrl = 'http://localhost:3000'; // Изменение пути для запросов к серверу
 const newsContainer = document.querySelector('#newsContainer');
 
 class NewsApp {
-  fetchNews(category) {
-    fetch(`${apiUrl}${category}&${apiKey}`)
-      .then((response) => {
-        if (!response.ok) return new Error('Ошибка запроса');
-        return response.json();
-      })
-      .then((result) => this.renderNews(result.articles))
-      .catch((error) => {
-        newsContainer.innerHTML = `<p class="error">${error.message}</p>`;
-      });
+  async fetchNews(category = 'general') {
+    try {
+      const response = await fetch(`${apiUrl}/news?category=${category}`); // Изменение пути запроса
+      if (!response.ok) {
+        throw new Error('Ошибка запроса');
+      }
+      const result = await response.json();
+      this.renderNews(result.articles);
+    } catch (error) {
+      console.error(error);
+      newsContainer.innerHTML = '<p>Не удалось загрузить новости. Пожалуйста, попробуйте позже.</p>';
+    }
   }
 
   renderNews(newsArray) {
     newsContainer.innerHTML = '';
     newsArray.forEach((el) => {
-      const newsItem = new NewsItem(el.title, el.description, el.urlToImage, el.url);
-      newsContainer.append(newsItem.render());
+      const article = document.createElement('a');
+      article.className = 'news-item';
+      article.href = el.url;
+      article.target = '_blank';
+      const artHeader = document.createElement('h2');
+      artHeader.className = 'news-title';
+      artHeader.textContent = el.title;
+      const description = document.createElement('p');
+      description.className = 'news-description';
+      description.textContent = el.description ? el.description : '';
+      const image = document.createElement('img');
+      image.className = 'image';
+      image.src = el.urlToImage ? el.urlToImage : '';
+      article.append(artHeader);
+      article.append(image);
+      article.append(description);
+      newsContainer.append(article);
     });
   }
 
   init() {
-    const tabs = document.querySelectorAll('.category-tab');
-    tabs.forEach((tab) => {
-      tab.addEventListener('click', (event) => {
-        const category = event.target.getAttribute('data-category');
-        this.fetchNews(category === undefined ? '' : category);
+    this.fetchNews();
+    const categoryTabs = document.querySelectorAll('.category-tab');
+    categoryTabs.forEach((tab) => {
+      tab.addEventListener('click', (e) => {
+        const category = e.target.getAttribute('data-category');
+        this.fetchNews(category || 'general');
       });
     });
-
-    this.fetchNews();
   }
 }
 
-class NewsItem {
-  constructor(title, description, src, href) {
-    this.title = title;
-    this.description = description;
-    this.src = src;
-    this.href = href;
-  }
-
-  render() {
-    const article = document.createElement('a');
-    article.className = 'news-item';
-    article.href = this.href;
-    article.target = '_blank';
-
-    const artHeader = document.createElement('h2');
-    artHeader.className = 'news-title';
-    artHeader.textContent = this.title;
-
-    const image = document.createElement('img');
-    image.className = 'image';
-    image.src = this.src ? this.src : '';
-
-    const description = document.createElement('p');
-    description.className = 'news-description';
-    description.textContent = this.description ? this.description : '';
-
-    article.append(artHeader);
-    article.append(image);
-    article.append(description);
-
-    return article;
-  }
-}
-
-const news = new NewsApp();
-
-news.init();
+const app = new NewsApp();
+app.init();
