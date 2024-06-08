1a7958b82af2273e306bc974b0b54eec32b36678
Denis Voinovich <denzelvojn@gmail.com>
2024-06-08 16:21:28 +0300

    build

--- src/index.js
+++ src/index.js
@@ -1,33 +1,31 @@
 const apiKey = 'apiKey=16cd62b7e752472586a561b2daab6ac9';
-const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&category=`;
+const apiUrl = 'https://newsapi.org/v2/top-headlines?country=us&category=';
 const newsContainer = document.querySelector('#newsContainer');
 
-
 class NewsApp {
-
   fetchNews(category) {
     fetch(`${apiUrl}${category}&${apiKey}`)
-      .then(response => {
-        if (!response.ok) return new Error('Ошибка запроса')
+      .then((response) => {
+        if (!response.ok) return new Error('Ошибка запроса');
         return response.json();
       })
-      .then(result => this.renderNews(result.articles))
-      .catch(error => {
+      .then((result) => this.renderNews(result.articles))
+      .catch((error) => {
         newsContainer.innerHTML = `<p class="error">${error.message}</p>`;
       });
   }
 
   renderNews(newsArray) {
     newsContainer.innerHTML = '';
-    newsArray.forEach(el => {
+    newsArray.forEach((el) => {
       const newsItem = new NewsItem(el.title, el.description, el.urlToImage, el.url);
       newsContainer.append(newsItem.render());
-    })
+    });
   }
 
   init() {
     const tabs = document.querySelectorAll('.category-tab');
-    tabs.forEach(tab => {
+    tabs.forEach((tab) => {
       tab.addEventListener('click', (event) => {
         const category = event.target.getAttribute('data-category');
         this.fetchNews(category === 'all' ? '' : category);
@@ -50,14 +48,14 @@ class NewsItem {
     const article = document.createElement('a');
     article.className = 'news-item';
     article.href = this.href;
-    article.target="_blank";
+    article.target = '_blank';
 
     const artHeader = document.createElement('h2');
     artHeader.className = 'news-title';
     artHeader.textContent = this.title;
 
     const image = document.createElement('img');
-    image.className = 'image'
+    image.className = 'image';
     image.src = this.src ? this.src : '';
 
     const description = document.createElement('p');
@@ -74,4 +72,4 @@ class NewsItem {
 
 const news = new NewsApp();
 
-news.init()
+news.init();
